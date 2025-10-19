35778f6493a9c13241792924fdd676d824d3bb5c
gitaj <arkiwestall@outlook.com>
2025-10-19 20:11:27 +1100

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-const ALCHEMY_RPC = process.env.ALCHEMY_RPC || "https://eth-sepolia.g.alchemy.com/v2/veSD0nysW7_16h_8Kb14l";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x511423562bdf8e6fe95cc5760fe78def1bb443e4a19a74711f0c9ac5e7c507cb";
+const ALCHEMY_RPC = process.env.ALCHEMY_RPC || "your alchemy RPC address here "; #this is priavate info hence not included 
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "your wallet private key here (the one from the sender address";#this is private info hence not included 
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
